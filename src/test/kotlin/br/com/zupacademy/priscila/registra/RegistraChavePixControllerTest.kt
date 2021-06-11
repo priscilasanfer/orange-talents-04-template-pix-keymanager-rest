dbfd203ab84dc36592ddb67aad2f8bafde1b386c
Priscila Ferreira <priscilasanfer@gmail.com>
2021-06-11 18:39:48 -0300

    031-testando-exposicao-de-registro-de-chave-pix

---
+++ src/test/kotlin/br/com/zupacademy/priscila/registra/RegistraChavePixControllerTest.kt
@@ -0,0 +1,67 @@
+package br.com.zupacademy.priscila.registra
+
+import br.com.zupacademy.priscila.KeyManagerRegistraServiceGrpc
+import br.com.zupacademy.priscila.RegistraChavePixResponse
+import br.com.zupacademy.priscila.shared.grpc.KeyManagerGrpcFactory
+import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Replaces
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito
+import java.util.*
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@MicronautTest
+internal class RegistraChavePixControllerTest {
+    @field:Inject
+    lateinit var registraStub: KeyManagerRegistraServiceGrpc.KeyManagerRegistraServiceBlockingStub
+
+    @field:Inject
+    @field:Client("/")
+    lateinit var client: HttpClient
+
+    @Test
+    internal fun `deve registrar uma nova chave pix`() {
+
+        val clienteId = UUID.randomUUID().toString()
+        val pixId = UUID.randomUUID().toString()
+
+        val respostaGrpc = RegistraChavePixResponse.newBuilder()
+            .setClientId(clienteId)
+            .setPixId(pixId)
+            .build()
+
+        given(registraStub.registra(Mockito.any())).willReturn(respostaGrpc)
+
+
+        val novaChavePix = NovaChavePixRequest(
+            tipoDeConta = TipoDeContaRequest.CONTA_CORRENTE,
+            chave = "teste@teste.com.br",
+            tipoDeChave = TipoDeChaveRequest.EMAIL
+        )
+
+        val request = HttpRequest.POST("/api/v1/clientes/$clienteId/pix", novaChavePix)
+        val response = client.toBlocking().exchange(request, NovaChavePixRequest::class.java)
+
+        assertEquals(HttpStatus.CREATED, response.status)
+        assertTrue(response.headers.contains("Location"))
+        assertTrue(response.header("Location")!!.contains(pixId))
+    }
+
+    @Factory
+    @Replaces(factory = KeyManagerGrpcFactory::class)
+    internal class MockitoStubFactory {
+
+        @Singleton
+        fun stubMock() =
+            Mockito.mock(KeyManagerRegistraServiceGrpc.KeyManagerRegistraServiceBlockingStub::class.java)
+    }
+
+}
